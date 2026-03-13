8b07257da74ee5ebd9b5bd2aa3148383da4c51e4
amkoopman1 <a.m.koopman.1@student.rug.nl>
2026-03-13 01:06:55 +0100

    new scripts on variation of body condition and weather

---
+++ scripts/04_condition-variation.R
@@ -0,0 +1,105 @@
+## body condition between groups and years
+
+# metadata samples Wender
+database_link <- "https://docs.google.com/spreadsheets/d/1ocuMq1ENjgwsVZ4uvhgyqwzb7D1d6v8HvuZTSrQ3kY0/edit"
+database <- read_gsdb(database_link)
+names(database)
+
+
+
+
+# adult captures data
+var_data <- database[["Captures"]] %>% select(Date, Species, Sexe, Age, Wing, Tarsus, Weight, Fat, Feces) %>%
+  mutate(Weight = as.numeric(Weight),
+         Wing = as.numeric(Wing),
+         Tarsus = as.numeric(Tarsus),
+         Fat = as.numeric(Fat),
+         Feces = str_sub(Feces, -6, -1),
+         day_of_year = yday(Date),  # Day of year (1-365)
+         days10 = ceiling((day_of_year -6) / 10), # the + changes the window of time
+         days21 = ceiling((day_of_year -6) / 21), # the + changes the window of time
+         days42 = ceiling((day_of_year +1) / 42), # the + changes the window of time
+         Year = substr(Date, 1,4)
+         )%>%
+  filter(str_sub(Feces, 1, 1) == "A" # make sure sample name is correct
+  )
+
+# for nestlings    
+
+var_nest_data <- database[["Nestlings"]] %>% select(Date, Species, Tarsus, Weight, Wing, Feces) %>%
+  mutate(Weight = as.numeric(Weight),
+         Wing = as.numeric(Wing),
+         Tarsus = as.numeric(Tarsus),
+         Feces = str_sub(Feces, -6, -1),
+         day_of_year = yday(Date),  # Day of year (1-365)
+         days10 = ceiling((day_of_year -6) / 10), # the + changes the window of time
+         days21 = ceiling((day_of_year -6) / 21), # the + changes the window of time
+         days42 = ceiling((day_of_year +1) / 42), # the + changes the window of time
+         Year = substr(Date, 1,4)
+  )%>%
+  filter(str_sub(Feces, 1, 1) == "A" # make sure sample name is correct
+  )
+
+
+
+
+# mean variables
+
+# choose between Weight, Wing, Tarsus, Fat (only for adult)
+input_var <- "Weight"
+# choose between days10, days21 and days42
+input_period <- "days10"
+# set minimal group size
+n_size <- 8
+
+var_data %>%
+  filter(Species %in% c('Rietzanger', 'Kleine karekiet'),
+         Year %in% c('2020', '2021','2022'),
+         Age == 'N1') %>%
+  group_by(Species, Year, .data[[input_period]]) %>% 
+  summarise(
+    n = n(),
+    mean_var = mean(.data[[input_var]], na.rm = TRUE),  
+    sd_var = sd(.data[[input_var]], na.rm = TRUE), 
+    se_var = sd_var / sqrt(n),  
+    ci_lower = mean_var - 1.96 * se_var,
+    ci_upper = mean_var + 1.96 * se_var
+  ) %>%
+  filter(n >= n_size) %>%
+  ggplot(aes(x = .data[[input_period]], y = mean_var, color = Species)) +  
+  geom_line(size = 1) +
+  geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2) +
+  geom_point(size = 2) +
+  facet_wrap(~Year, ncol = 3) +
+  scale_x_continuous(
+    breaks = function(x) seq(floor(min(x)), ceiling(max(x)), by = 1)) +
+  labs(x = input_period, y = input_var) +
+  theme_minimal()
+  
+  
+  
+
+# nestling var
+  var_nest_data %>%
+    filter(Species %in% c('Rietzanger', 'Kleine karekiet'),
+           Year %in% c('2020', '2021','2022')) %>%
+    group_by(Species, Year, .data[[input_period]]) %>% 
+    summarise(
+      n = n(),
+      mean_var = mean(.data[[input_var]], na.rm = TRUE),  
+      sd_var = sd(.data[[input_var]], na.rm = TRUE), 
+      se_var = sd_var / sqrt(n),  
+      ci_lower = mean_var - 1.96 * se_var,
+      ci_upper = mean_var + 1.96 * se_var
+    ) %>%
+    filter(n >= n_size) %>%
+    ggplot(aes(x = .data[[input_period]], y = mean_var, color = Species)) +  
+    geom_line(size = 1) +
+    geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2) +
+    geom_point(size = 2) +
+    facet_wrap(~Year, ncol = 3) +
+    scale_x_continuous(
+      breaks = function(x) seq(floor(min(x)), ceiling(max(x)), by = 1)) +
+    labs(x = input_period, y = input_var) +
+    theme_minimal()
+  
---
+++ scripts/05_weather-variation.R
@@ -0,0 +1,204 @@
+## weather variation
+
+
+# read the data
+
+# rain
+rain_url <- "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_270_rh24.txt"
+
+a_temp_url <- "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_270_tg.txt"
+
+min_temp_url <- "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_270_tng.txt"
+
+max_temp_url <- "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_270_txg.txt"
+
+atmos_url <- "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/maandgegevens/mndgeg_270_pg.txt"
+
+
+# rain data
+rain_data <- read_csv(rain_url, 
+                         skip = 20,  # skip header lines
+                         col_names = c("STN", "YYYY", "JAN", "FEB", "MAR", "APR", 
+                                       "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", 
+                                       "NOV", "DEC", "YEAR"),
+                         na = c("", "NA", "     "))  # handle missing values
+
+
+head(rain_data)
+
+
+# Convert to long format
+rain_long <- rain_data %>%
+  pivot_longer(cols = JAN:YEAR,
+               names_to = "Month",
+               values_to = "Rain") %>%
+  mutate(Rain = as.numeric(Rain) / 10, # convert to mm
+         YYYY = as.numeric(YYYY)) %>% # treat  year as numeric
+  filter(YYYY >= 1995 & YYYY != 2026) # filter for last 30 years
+ 
+
+
+# average temp
+a_temp_data <- read_csv(a_temp_url, 
+                      skip = 20,  # Skip header lines
+                      col_names = c("STN", "YYYY", "JAN", "FEB", "MAR", "APR", 
+                                    "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", 
+                                    "NOV", "DEC", "YEAR"),
+                      na = c("", "NA", "     "))  # Handle missing values
+
+head(a_temp_data)
+
+
+# Convert to long format
+a_temp_long <- a_temp_data %>%
+  pivot_longer(cols = JAN:YEAR,
+               names_to = "Month",
+               values_to = "avg_temp") %>%
+  mutate(avg_temp = as.numeric(avg_temp) / 10, # convert to degrees
+         YYYY = as.numeric(YYYY)) %>% # treat  year as numeric
+  filter(YYYY >= 1995 & YYYY != 2026) # filter for last 30 years
+
+
+# minimum temp
+min_temp_data <- read_csv(min_temp_url, 
+                        skip = 20,  # Skip header lines
+                        col_names = c("STN", "YYYY", "JAN", "FEB", "MAR", "APR", 
+                                      "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", 
+                                      "NOV", "DEC", "YEAR"),
+                        na = c("", "NA", "     "))  # Handle missing values
+
+head(min_temp_data)
+
+
+# Convert to long format
+min_temp_long <- min_temp_data %>%
+  pivot_longer(cols = JAN:YEAR,
+               names_to = "Month",
+               values_to = "min_temp") %>%
+  mutate(min_temp = as.numeric(min_temp) / 10, # convert to degrees
+         YYYY = as.numeric(YYYY)) %>% # treat  year as numeric
+  filter(YYYY >= 1995 & YYYY != 2026) # filter for last 30 years
+
+
+# maximum temp
+max_temp_data <- read_csv(max_temp_url, 
+                        skip = 20,  # Skip header lines
+                        col_names = c("STN", "YYYY", "JAN", "FEB", "MAR", "APR", 
+                                      "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", 
+                                      "NOV", "DEC", "YEAR"),
+                        na = c("", "NA", "     "))  # Handle missing values
+
+head(max_temp_data)
+
+
+# Convert to long format
+max_temp_long <- max_temp_data %>%
+  pivot_longer(cols = JAN:YEAR,
+               names_to = "Month",
+               values_to = "max_temp") %>%
+  mutate(max_temp = as.numeric(max_temp) / 10, # convert to degrees
+         YYYY = as.numeric(YYYY)) %>% # treat  year as numeric
+  filter(YYYY >= 1995 & YYYY != 2026) # filter for last 30 years
+
+
+
+# atmospheric pressure
+atmos_data <- read_csv(atmos_url, 
+                      skip = 20,  # skip header lines
+                      col_names = c("STN", "YYYY", "JAN", "FEB", "MAR", "APR", 
+                                    "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", 
+                                    "NOV", "DEC", "YEAR"),
+                      na = c("", "NA", "     "))  # handle missing values
+
+
+head(atmos_data)
+
+
+# Convert to long format
+atmos_long <- atmos_data %>%
+  pivot_longer(cols = JAN:YEAR,
+               names_to = "Month",
+               values_to = "hPa") %>%
+  mutate(hPa = as.numeric(hPa) / 10, # convert to mm
+         YYYY = as.numeric(YYYY)) %>% # treat  year as numeric
+  filter(YYYY >= 1995 & YYYY != 2026) # filter for last 30 years
+
+
+# rain graph
+rain_long %>%
+  group_by(Month) %>%
+  summarise(
+    variability = sd(Rain, na.rm = TRUE),
+    cv = sd(Rain, na.rm = TRUE) / mean(Rain, na.rm = TRUE)
+  ) %>%
+  mutate(Month = factor(Month, levels = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                                          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "YEAR"))) %>%
+  ggplot(aes(x = Month, y = cv)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Rain variability (between years) of 1995-2025",
+       y = "coefficient of variation (CV)",
+       x = "period") +
+  theme_minimal()
+
+
+# average temp graph
+a_temp_long %>%
+  group_by(Month) %>%
+  summarise(
+    variability = sd(avg_temp, na.rm = TRUE))%>%
+  mutate(Month = factor(Month, levels = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                                          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "YEAR"))) %>%
+  ggplot(aes(x = Month, y = variability)) +
+  geom_col(fill = "orange") +
+  labs(title = "avg_temp variability (between years) of 1995-2025",
+       y = "variability (sd)",
+       x = "period") +
+  ylim(0, 2.5) +
+  theme_minimal()
+
+
+# minimum temp graph
+min_temp_long %>%
+  group_by(Month) %>%
+  summarise(
+    variability = sd(min_temp, na.rm = TRUE))%>%
+  mutate(Month = factor(Month, levels = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                                          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "YEAR"))) %>%
+  ggplot(aes(x = Month, y = variability)) +
+  geom_col(fill = "orange") +
+  labs(title = "min_temp variability (between years) of 1995-2025",
+       y = "variability (sd)",
+       x = "period") +
+  ylim(0, 2.5) +
+  theme_minimal()
+
+# maximum temp graph
+max_temp_long %>%
+  group_by(Month) %>%
+  summarise(
+    variability = sd(max_temp, na.rm = TRUE))%>%
+  mutate(Month = factor(Month, levels = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                                          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "YEAR"))) %>%
+  ggplot(aes(x = Month, y = variability)) +
+  geom_col(fill = "orange") +
+  labs(title = "max_temp variability (between years) of 1995-2025",
+       y = "variability (sd)",
+       x = "period") +
+  ylim(0, 2.5) +
+  theme_minimal()
+
+# atmospheric pressure graph
+atmos_long %>%
+  group_by(Month) %>%
+  summarise(
+    variability = sd(hPa, na.rm = TRUE),
+    cv = sd(hPa, na.rm = TRUE) / mean(hPa, na.rm = TRUE)
+  ) %>%
+  mutate(Month = factor(Month, levels = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                                          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "YEAR"))) %>%
+  ggplot(aes(x = Month, y = variability)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "hPa variability (between years) of 1995-2025",
+       y = "variability (sd)",
+       x = "period") +
+  theme_minimal()
